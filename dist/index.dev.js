b4ee6ca6799add9c3f20682c423ddcbe772456ea
lucy gilhooly <lucy_johannah@hotmail.com>
2022-08-24 15:28:52 +0100

    created the appropriate files and started notes

---
+++ dist/index.dev.js
@@ -0,0 +1,10 @@
+//when u visit website we want to have a homescreen with a button reading 'click here to play!'
+// will need a function which changes the screen to the next (can possibly be reused when hitting the next question button)
+// question screen should display an image and 4 options, one of them being correct. 
+// will store all questions within an array as objects
+//if else statement with booleans for correct or incorrect answers 
+//figure out how to keep score? 
+//maybe add function to an event listener that if correct answer is pressed a point is recorded 
+//this would be stored in an array and should be displayed onscreen too
+//also want a home button to permanantly be displayed at the top 
+"use strict";
