c2ba261f8bc1c4b2ef27be4a8d2a972021f9bf2d
lucy gilhooly <lucy_johannah@hotmail.com>
2022-08-25 16:49:04 +0100

    screen now clears and displays first Q

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -3,12 +3,23 @@
 //CLICK TO PLAY BUTTON
 //FIRST, grab from HTML
 var playButton = document.querySelector('#start-btn');
-var questionContainer = document.querySelector(".container__question"); //then, create a function for it to perform
+var questionContainer = document.querySelector(".container__question");
+var actualQuestion = document.querySelector('#question');
+var answerButtons = document.querySelector('.btn-grid');
+var nextButton = document.querySelector('#next-btn'); // new undefined variables to help with ordering questions
+
+var shuffledQuestions = undefined;
+var currentQuestionIndex = undefined; //then, create a function for it to perform
 // so here the screen will clear and the title will be replaced with a question 
 
 var startGame = function startGame() {
   console.log("let the games begin!");
   playButton.classList.add('hide');
+  shuffledQuestions = questions.sort(function () {
+    return Math.random();
+  }); //possibly add in -.5, to give us a number above or below zero 50% of the time. Unsure if necessary
+
+  currentQuestionIndex = 0;
   questionContainer.classList.remove('hide');
   setNextQuestion();
 }; //& then create an event listener so when button is pressed the function is executed 
@@ -16,11 +27,57 @@ var startGame = function startGame() {
 
 playButton.addEventListener("click", startGame); // and the button will be replaced with 4 buttons with multi choice answers !!!
 //QUESTIONS 
-//const setNextQuestion() => {
-//}
-// const selectAnswer() => {
-// }
-// will need a function which changes the screen to the next (can possibly be reused when hitting the next question button)
+
+var questions = [{
+  question: "what is my name",
+  answers: [{
+    text: 'Lucy',
+    correct: true
+  }, {
+    text: 'Sam',
+    correct: false
+  }, {
+    text: 'Lottie',
+    correct: false
+  }, {
+    text: 'Andrew',
+    correct: false
+  }]
+}];
+
+var setNextQuestion = function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+var showQuestion = function showQuestion(question) {
+  actualQuestion.innerText = question.question;
+  question.answers.forEach(function (answer) {
+    var button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
+    answerButtons.appendChild(button);
+  });
+};
+
+var resetState = function resetState() {
+  nextButton.classList.add('hide');
+
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+};
+
+var selectAnswer = function selectAnswer(event) {
+  var selectedButton = event.target;
+  var correct = selectedButton.dataset.correct;
+}; // will need a function which changes the screen to the next (can possibly be reused when hitting the next question button)
 // question screen should display an image and 4 options, one of them being correct. 
 // will store all questions within an array as objects
 //if else statement with booleans for correct or incorrect answers 
--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 
 const playButton = document.querySelector('#start-btn');
 const questionContainer = document.querySelector(".container__question");
+const actualQuestion = document.querySelector('#question');
+const answerButtons = document.querySelector('.btn-grid');
+const nextButton = document.querySelector('#next-btn');
+
+// new undefined variables to help with ordering questions
+let shuffledQuestions = undefined
+let currentQuestionIndex = undefined
 
 //then, create a function for it to perform
 // so here the screen will clear and the title will be replaced with a question 
 const startGame = () => {
     console.log("let the games begin!")
     playButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() ); //possibly add in -.5, to give us a number above or below zero 50% of the time. Unsure if necessary
+    currentQuestionIndex = 0 ;
     questionContainer.classList.remove('hide');
     setNextQuestion()
-
 }
 
 //& then create an event listener so when button is pressed the function is executed 
@@ -23,13 +31,50 @@ playButton.addEventListener("click", startGame);
 
 //QUESTIONS 
 
-//const setNextQuestion() => {
-    
-//}
+const questions = [
+    {question: "what is my name",
+        answers: [
+            { text: 'Lucy', correct: true },
+            { text: 'Sam', correct: false },
+            {text: 'Lottie', correct: false},
+            {text: 'Andrew', correct: false}
+            ]
+    }
+]
 
-// const selectAnswer() => {
 
-// }
+const setNextQuestion = () => {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+const showQuestion = (question) => {
+    actualQuestion.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+        answerButtons.appendChild(button);
+    });
+
+}
+
+const resetState = () => {
+    nextButton.classList.add('hide');
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+const selectAnswer = (event) => {
+    const selectedButton = event.target;
+    const correct = selectedButton.dataset.correct;
+}
 
 
 
