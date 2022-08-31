93d3e8ad71261e26f0dc5f06eb20147e755607fc
lucy gilhooly <lucy_johannah@hotmail.com>
2022-08-31 22:38:52 +0100

    corrected spelling

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const questions = [{
                 correct: false
             },
             {
-                text: 'Dolpihns',
+                text: 'Dolphins',
                 correct: false
             },
             {
